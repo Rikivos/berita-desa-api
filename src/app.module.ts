a627115c27a01b305c42d3a22a1f3aada2dd4b7a
Rikivos <rizkypenpenz.com>
2025-07-05 16:38:31 +0700

    Feat : add dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [CategoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!), // ← pakai tanda ! jika kamu yakin
+    CategoryModule,
+  ],
 })
 export class AppModule {}
