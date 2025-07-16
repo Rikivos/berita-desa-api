a124d8b741323fc0e6bb95258084c2fa686cb51a
Rikivos <rizkypenpenz@gmail.com>
2025-07-16 20:29:21 +0700

    delete readme in backend-v2

--- api-test/test.js
+++ api-test/test.js
@@ -2,17 +2,16 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 10, // 20 virtual users (simulasi 20 orang)
-  duration: '10s', // durasi tes
+  vus: 10, 
+  duration: '10s',
 };
 
 export default function () {
-  const res = http.get('http://localhost:3000/api/posts?page=1');
+  const res = http.get('http://localhost:3000/api/posts');
 
-  // Cek respons sukses
   check(res, {
     'status 200': (r) => r.status === 200,
   });
 
-  sleep(1); // jeda antar request (simulasi pengguna manusia)
+  sleep(1); 
 }
